bf63a149ffb1859b5e8c2cec07fd9eba01529099
hpcatley <hpcatley@gmail.com>
2023-10-15 13:47:52 +1030

    2

--- klargest.cpp
+++ klargest.cpp
@@ -2,10 +2,12 @@
 #include <queue>
 
 int kth_largest(std::vector<int> values, int k) {
-    if(k <= 0) {
-        return -1;
+    if(k <= 0 || values.size()<k) {
+        throw std::length_error("error");
     }
-    std::priority_queue<int> priorityQ;
+    std::priority_queue<int, std::vector<int>, std::greater<int>> priorityQ;
+    // https://en.cppreference.com/w/cpp/container/priority_queue#:~:text=A%20priority%20queue%20is%20a,appear%20as%20the%20top(). 
+    // used above link to aid me with this bit
     for (int value : values) {
         if(priorityQ.size() < k) {
             priorityQ.push(value); 
